3540c41bb9dc657df4d88baf3f9d0eb3f79392ab
Jasper De Keukelaere (imec) <Jasper.DeKeukelaere@imec.be>
2024-01-15 19:55:35 +0100

    chore: move notification to bottom right

--- PLCsimAdvanced_Manager/Program.cs
+++ PLCsimAdvanced_Manager/Program.cs
@@ -12,7 +12,17 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddBootstrapBlazor();
 builder.Services.AddMatBlazor();
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+    {
+        config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+        config.SnackbarConfiguration.NewestOnTop = false;
+        config.SnackbarConfiguration.ShowCloseIcon = true;
+        config.SnackbarConfiguration.VisibleStateDuration = 10000;
+        config.SnackbarConfiguration.HideTransitionDuration = 500;
+        config.SnackbarConfiguration.ShowTransitionDuration = 500;
+        config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+    }
+);
 var useUrls = builder.Configuration.GetSection("UseUrls").Get<string[]>();
 if (useUrls != null && useUrls.Length > 0)
 {
